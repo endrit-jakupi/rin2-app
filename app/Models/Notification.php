6f95db35d29ed00e4405dbefac0bf62f0c76e292
Endrit <endritjakupi08@gmail.com>
2026-09-10 09:05:42 +0200

    feat: add notification model

---
+++ app/Models/Notification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'message',
+        'expires_at',
+        'user_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'expires_at' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reads(): HasMany
+    {
+        return $this->hasMany(NotificationRead::class);
+    }
+}
