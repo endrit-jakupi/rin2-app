cf200e6a80049b69034a94b5ed9629d27ccc402b
Endrit <endritjakupi08@gmail.com>
2026-09-10 15:19:04 +0200

    factor: add UI details for users page

--- resources/views/components/pages/⚡users.blade.php
+++ resources/views/components/pages/⚡users.blade.php
@@ -56,17 +56,17 @@ new class extends Component
                 <tr
                     wire:click="impersonate({{ $user->id }})"
                     title="Impersonate"
-                    class="cursor-pointer border-b"
+                    class="cursor-pointer border-b hover:bg-black hover:text-white"
                 >
                     <td class="px-4 py-4 font-medium">
                         {{ $user->name }}
                     </td>
 
-                    <td class="px-4 py-4 text-gray-600">
+                    <td class="px-4 py-4">
                         {{ $user->email }}
                     </td>
 
-                    <td class="px-4 py-4 text-gray-600">
+                    <td class="px-4 py-4">
                         {{ $user->unread_notifications_count }}
                     </td>
                 </tr>
