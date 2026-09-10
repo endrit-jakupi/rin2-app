071af828415dae93dd13d4e58955f8dd96f176e0
Endrit <endritjakupi08@gmail.com>
2026-09-10 13:07:16 +0200

    feat: add notifications and phone to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'email',
+    'password',
+    'notifications_enabled',
+    'phone',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
