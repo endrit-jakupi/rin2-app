c69d87829b311162f7660e2892093e6372730ea3
Endrit <endritjakupi08@gmail.com>
2026-09-10 08:59:46 +0200

    feat: add notifications table

---
+++ database/migrations/2026_09_10_065642_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('type');
+            $table->text('message');
+            $table->timestamp('expires_at');
+
+            $table->foreignId('user_id')
+                ->nullable() // Necessary because a global notification doesn't have one specific user 
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
